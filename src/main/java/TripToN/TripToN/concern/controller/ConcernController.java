d5c83b22a12ab1cb5ca0da8e0652cd9653c1b536
nippyclouding <wonderwall09in@naver.com>
2026-04-09 21:29:55 +0900

    pull

---
+++ src/main/java/TripToN/TripToN/concern/controller/ConcernController.java
@@ -0,0 +1,11 @@
+package TripToN.TripToN.concern.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/concern")
+public class ConcernController {
+}
